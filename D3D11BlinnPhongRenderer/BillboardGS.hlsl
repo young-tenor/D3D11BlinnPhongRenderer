aecd0d6f8d19a6598d4289c05845d3634f6a2ccf
young-tenor <ytenor03@gmail.com>
2026-03-13 15:03:25 +0900

    [Billboard] 텍스쳐 좌표 구현

--- D3D11BlinnPhongRenderer/BillboardGS.hlsl
+++ D3D11BlinnPhongRenderer/BillboardGS.hlsl
@@ -8,6 +8,7 @@ struct GSInput {
 
 struct PSInput {
     float4 pos : SV_POSITION;
+    float2 texcoord : TEXCOORD;
 };
 
 [maxvertexcount(4)]
@@ -15,14 +16,21 @@ void main(point GSInput input[1], inout TriangleStream<PSInput> outStream) {
     float size = 0.5f;
     
     float4 v[4];
-    v[0] = input[0].pos + float4(-size, -size, 0.0f, 0.0f);
-    v[1] = input[0].pos + float4(-size, size, 0.0f, 0.0f);
-    v[2] = input[0].pos + float4(size, -size, 0.0f, 0.0f);
-    v[3] = input[0].pos + float4(size, size, 0.0f, 0.0f);
+    v[0] = input[0].pos + float4(-size, size, 0.0f, 0.0f);
+    v[1] = input[0].pos + float4(size, size, 0.0f, 0.0f);
+    v[2] = input[0].pos + float4(-size, -size, 0.0f, 0.0f);
+    v[3] = input[0].pos + float4(size, -size, 0.0f, 0.0f);
+    
+    float2 texcoord[4];
+    texcoord[0] = float2(0.0f, 0.0f);
+    texcoord[1] = float2(1.0f, 0.0f);
+    texcoord[2] = float2(0.0f, 1.0f);
+    texcoord[3] = float2(1.0f, 1.0f);
 
     PSInput output;
     for (int i = 0; i < 4; i++) {
         output.pos = mul(v[i], viewProj);
+        output.texcoord = texcoord[i];
         outStream.Append(output);
     }
 }
--- D3D11BlinnPhongRenderer/BillboardPS.hlsl
+++ D3D11BlinnPhongRenderer/BillboardPS.hlsl
@@ -1,7 +1,9 @@
 struct PSInput {
     float4 pos : SV_POSITION;
+    float2 texcoord : TEXCOORD;
 };
 
 float4 main(PSInput input) : SV_TARGET {
-    return float4(1.0f, 0.0f, 0.0f, 1.0f);
+    return float4(input.texcoord, 0.0f, 1.0f);
+
 }
