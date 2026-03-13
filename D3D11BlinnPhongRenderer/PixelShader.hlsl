48332cf98ff3869cbcee8545cda55e45eb67a209
young-tenor <ytenor03@gmail.com>
2026-03-13 12:41:17 +0900

    변수 이름을 Frank D. Luna스럽게 변경

--- D3D11BlinnPhongRenderer/PixelShader.hlsl
+++ D3D11BlinnPhongRenderer/PixelShader.hlsl
@@ -47,20 +47,20 @@ Texture2D g_texture : register(t0);
 SamplerState g_sampler : register(s0);
 
 // https://en.wikipedia.org/wiki/Blinn%E2%80%93Phong_reflection_model
-float3 BlinnPhong(float3 normal, float3 lightVec, float3 lightStrength, float3 eyeVec) {
-    float3 halfway = normalize(lightVec + eyeVec);
+float3 BlinnPhong(float3 normal, float3 lightVec, float3 lightStrength, float3 toEye) {
+    float3 halfway = normalize(lightVec + toEye);
     float3 specular = material.specular * pow(max(dot(normal, halfway), 0.0f), material.shininess);
     
     return material.ambient + (material.diffuse + specular) * lightStrength;
 }
 
-float3 DirectionalLight(float3 normal, float3 eyeVec) {
+float3 DirectionalLight(float3 normal, float3 toEye) {
     float3 lightVec = -light.dir;
     float3 lightStrength = light.strength * max(dot(lightVec, normal), 0.0f);
-    return BlinnPhong(normal, lightVec, lightStrength, eyeVec);
+    return BlinnPhong(normal, lightVec, lightStrength, toEye);
 }
 
-float3 PointLight(float3 normal, float3 pos, float3 eyeVec) {
+float3 PointLight(float3 normal, float3 pos, float3 toEye) {
     float3 lightVec = light.pos - pos;
     
     float dist = length(lightVec);
@@ -74,11 +74,11 @@ float3 PointLight(float3 normal, float3 pos, float3 eyeVec) {
         float attenuation = saturate((light.fallOffEnd - dist) / (light.fallOffEnd - light.fallOffStart));
         lightStrength *= attenuation;
         
-        return BlinnPhong(normal, lightVec, lightStrength, eyeVec);
+        return BlinnPhong(normal, lightVec, lightStrength, toEye);
     }
 }
 
-float3 SpotLight(float3 normal, float3 pos, float3 eyeVec) {
+float3 SpotLight(float3 normal, float3 pos, float3 toEye) {
     float3 lightVec = light.pos - pos;
     
     float dist = length(lightVec);
@@ -96,20 +96,20 @@ float3 SpotLight(float3 normal, float3 pos, float3 eyeVec) {
         float spotFactor = pow(max(dot(-lightVec, light.dir), 0.0f), light.spotPower);
         lightStrength *= spotFactor;
         
-        return BlinnPhong(normal, lightVec, lightStrength, eyeVec);
+        return BlinnPhong(normal, lightVec, lightStrength, toEye);
     }
 }
 
 float4 main(PSInput input) : SV_TARGET {
-    float3 eyeVec = normalize(eyePos - input.posWorld);
+    float3 toEye = normalize(eyePos - input.posWorld);
     
     float3 color = float3(0.0f, 0.0f, 0.0f);
     if (light.type == DIRECTIONAL) {
-        color += DirectionalLight(input.normal, eyeVec);
+        color += DirectionalLight(input.normal, toEye);
     } else if (light.type == POINT) {
-        color += PointLight(input.normal, input.posWorld, eyeVec);
+        color += PointLight(input.normal, input.posWorld, toEye);
     } else if (light.type == SPOT) {
-        color += SpotLight(input.normal, input.posWorld, eyeVec);
+        color += SpotLight(input.normal, input.posWorld, toEye);
     } else {
         color += float3(1.0f, 0.0f, 0.0f);
     }
