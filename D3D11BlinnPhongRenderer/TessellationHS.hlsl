369cf50c7962e6d8fac29cd3e8c4ec67b8373c41
young-tenor <ytenor03@gmail.com>
2026-03-20 14:34:09 +0900

    patch culling 구현

--- D3D11BlinnPhongRenderer/TessellationHS.hlsl
+++ D3D11BlinnPhongRenderer/TessellationHS.hlsl
@@ -23,8 +23,32 @@ struct HSConstOutput {
 
 HSConstOutput PatchConstFunc(InputPatch<VSOutput, 4> patch, uint patchId : SV_PrimitiveID) {
     HSConstOutput output;
-    output.edges[0] = output.edges[1] = output.edges[2] = output.edges[3] = tess;
-    output.inside[0] = output.inside[1] = tess;
+
+    float3 n0 = normalize(patch[0].pos);
+    float3 n1 = normalize(patch[1].pos);
+    float3 n2 = normalize(patch[2].pos);
+    float3 n3 = normalize(patch[3].pos);
+
+    float3 v0 = normalize(eyePos - patch[0].pos);
+    float3 v1 = normalize(eyePos - patch[1].pos);
+    float3 v2 = normalize(eyePos - patch[2].pos);
+    float3 v3 = normalize(eyePos - patch[3].pos);
+    
+    float dot0 = dot(n0, v0);
+    float dot1 = dot(n1, v1);
+    float dot2 = dot(n2, v2);
+    float dot3 = dot(n3, v3);
+    
+    float maxDot = max(max(dot0, dot1), max(dot2, dot3));
+    
+    if (maxDot < -0.1f) {
+        output.edges[0] = output.edges[1] = output.edges[2] = output.edges[3] = 0.0f;
+        output.inside[0] = output.inside[1] = 0.0f;
+    } else {
+        output.edges[0] = output.edges[1] = output.edges[2] = output.edges[3] = tess;
+        output.inside[0] = output.inside[1] = tess;
+    }
+
     return output;
 }
 
